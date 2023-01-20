c9f9014c679d7ac9ae5328a6bfedee835d4cdca0
hudahamid <huda.00091581@gmail.com>
2023-01-20 20:19:58 +0300

    done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+const destructivelyAppendCat = (name) => {
+  cats.push(name);
+};
+const destructivelyPrependCat = (name) => {
+  cats.unshift(name);
+};
+const destructivelyRemoveLastCat = () => {
+  cats.pop();
+};
+const destructivelyRemoveFirstCat = () => {
+  cats.shift();
+};
+const appendCat = (name) => {
+  const arr = [...cats];
+  arr.push(name);
+  return arr;
+};
+const prependCat = (name) => {
+  const arr = [...cats];
+  arr.unshift(name);
+  return arr;
+};
+const removeLastCat = () => {
+  const arr = [...cats];
+  arr.pop();
+  return arr;
+};
+const removeFirstCat = () => {
+  const arr = [...cats];
+  arr.shift();
+  return arr;
+};
